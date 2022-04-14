880800b73297a007ad6fc631531e7fa665d56b26
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-04-14 10:10:14 +0200

    add display todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./style.css";
+
+const ul = document.querySelector("ul");
+
+const todos = [
+  {
+    text: "I'am a todo",
+    done: false,
+  },
+  {
+    text: "to do JavaScript",
+    done: true,
+  },
+];
+
+const displayTodo = () => {
+  const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo, index);
+  });
+  ul.innerHTML = "";
+  ul.append(...todosNode);
+};
+
+const createTodoElement = (todo, index) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+        <span class="todo ${todo.done ? "done" : ""}"></span>
+        <p>${todo.text}</p>
+        <button>Delete</button>
+    `;
+  return li;
+};
+
+displayTodo();
